b5b19c3e96bedec815db5af39b853494359206b7
tEdFAn026 <a2857782qq@163.com>
2018-12-08 07:11:37 +0000

    Finish category
    Finish category

--- src/co7219/as3/zf41/Category.java
+++ src/co7219/as3/zf41/Category.java
@@ -1,3 +1,24 @@
+/*
+ * zf41 solution to CO7219 Assignment 3, Task 1 The mapper extracts the
+ * 
+ * relevant fields from the input line and emits a key-value pair with the
+ * category as key and a String containing four numbers separated by commas
+ * as value: number of free apps, 0, number of paid apps, total paid apps
+ * price. Two of the four numbers are always 0 because each call of the map
+ * method processes either free or paid apps.
+ * 
+ * The combiner aggregates the values for each category by calculating the
+ * sum of each of the four components.
+ * 
+ * The reducer does the same aggregation as the combiner and then calculates
+ * the average paid apps of arrivals by dividing the number of paid apps,
+ * and similarly for departures.
+ * 
+ * please input arguments: 
+ * -output "output path" 	set output path
+ * -data "data source path"	set data source path
+ */
+
 package co7219.as3.zf41;
 
 import java.io.IOException;
@@ -24,18 +45,55 @@ public class Category {
 		private Text category = new Text();
 
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-//			String[] lineElements = splitbycomma(value.toString());
+			// String[] lineElements = splitbycomma(value.toString());
 
-			String csvSplitBy = ",(?=([^\"]*\"[^\"]*\")*[^\"]*$)"; 
-			
+			// regex for split string from csv
+			String csvSplitBy = ",(?=([^\"]*\"[^\"]*\")*[^\"]*$)";
+
 			String[] lineElements = value.toString().split(csvSplitBy);
-			
+
+			// skip empty lines and header lines
 			if (lineElements.length == 0 || lineElements[0].equals("App"))
 				return;
 
+			// create value v to be output in the format
+			// (num_Free,0,num_Paid,Paid_price)
+			String emitValue;
+			if (lineElements[6].trim().equals("Free"))
+				emitValue = "1,0,0,0";
+			else if (lineElements[6].trim().equals("Paid"))
+				emitValue = "0,0,1," + lineElements[7];
+			else
+				return;
+
+			// emit
 			category.set(lineElements[1].trim());
-			value.set(lineElements[6] + "," + lineElements[7]);
+			value.set(emitValue);
 			context.write(category, value);
+		}
+	}
+
+	public static class CategoryCombiner extends Reducer<Text, Text, Text, Text> {
+		Text result = new Text();
+
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+			int sumfree = 0; // number of free apps
+			int sumPaid = 0; // number of paid apps
+			// I use BigDecimal instead of double or float because for avoiding
+			// loss of precision
+			BigDecimal sumPrice = new BigDecimal("0"); // total price
+
+			for (Text val : values) {
+				String[] v = val.toString().split(",");
+				sumfree += Integer.parseInt(v[0]);
+				sumPaid += Integer.parseInt(v[2]);
+				sumPrice = sumPrice.add(new BigDecimal((v[3].trim().replace("$", ""))));
+			}
+
+			// emit one key-value pair with the aggregated values
+			result.set(sumfree + ",0," + sumPaid + "," + sumPrice.toString());
+			context.write(key, result);
+
 		}
 	}
 
@@ -44,48 +102,74 @@ public class Category {
 
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
-			int sumfree = 0;
-			int sumPaid = 0;
-			BigDecimal sumPrice = new BigDecimal("0");
+			// do the same aggregation as in the combiner
+			int sumfree = 0; // number of free apps
+			int sumPaid = 0; // number of paid apps
+			BigDecimal sumPrice = new BigDecimal("0"); // total price
 			// ArrayList<Float> price = new ArrayList<Float>();
 			for (Text val : values) {
 				String[] v = val.toString().split(",");
-				if (v[0].trim().equals("Free"))
-					sumfree++;
-				if (v[0].trim().equals("Paid")) {
-					sumPaid++;
-					// price.add(Float.parseFloat(v[1]));
-//					System.out.print(sumPrice +"+"+""+v[1].trim().replace("$", "")+"=");
-					
-					sumPrice = sumPrice.add(new BigDecimal((v[1].trim().replace("$", ""))));
-//					System.out.println(sumPrice);
-				}
+				// if (v[0].trim().equals("Free"))
+				// sumfree++;
+				// if (v[0].trim().equals("Paid")) {
+				// sumPaid++;
+				// // price.add(Float.parseFloat(v[1]));
+				// // System.out.print(sumPrice
+				// // +"+"+""+v[1].trim().replace("$", "")+"=");
+				//
+				// sumPrice = sumPrice.add(new
+				// BigDecimal((v[1].trim().replace("$", ""))));
+				// // System.out.println(sumPrice);
+				// }
+				sumfree += Integer.parseInt(v[0]);
+				sumPaid += Integer.parseInt(v[2]);
+				sumPrice = sumPrice.add(new BigDecimal((v[3].trim().replace("$", ""))));
 			}
 
+			// compute average delays and output them
 			result.set(sumfree + ", " + sumPaid + ", "
 					+ div(sumPrice, new BigDecimal(String.valueOf(sumPaid)), 2).toString());
 			context.write(key, result);
 		}
 
+		/**
+		 * Provides (relatively) precise division. When an inexhaustible
+		 * situation occurs, the precision is specified by the scale parameter,
+		 * and the subsequent numbers are rounded off.
+		 * 
+		 * @param v1
+		 *            is divisor
+		 * @param v2
+		 *            divisor
+		 * @param scale
+		 *            indicates that it needs to be accurate to a few decimal
+		 *            places.
+		 * @return quotient of two parameters
+		 */
 		public static BigDecimal div(BigDecimal b1, BigDecimal b2, int scale) {
 
-			if (b2.compareTo(BigDecimal.ZERO)==0)
+			// Returns 0 if the denominator is 0
+			if (b2.compareTo(BigDecimal.ZERO) == 0)
 				return b2.setScale(scale, BigDecimal.ROUND_HALF_UP);
-			
+
 			return b1.divide(b2, scale, BigDecimal.ROUND_HALF_UP);
 		}
 	}
 
-	public enum CMD {
+	// Settings for now this program only have two option setting, datapath and
+	// output path
+	public enum SettingType {
 		cmd_dataPath, cmd_outputPath
 	}
 
-	private static EnumMap<CMD, String> settings = new EnumMap<CMD, String>(CMD.class);{
-		{
-			settings.put(CMD.cmd_dataPath, "GooglePlayStoreDataTest");
-			settings.put(CMD.cmd_outputPath, "output/category");
-		}
-	};
+	// store settings
+	private static EnumMap<SettingType, String> settings = new EnumMap<SettingType, String>(SettingType.class);
+	// {
+	// {
+	// settings.put(SettingType.cmd_dataPath, "GooglePlayStoreDataTest");
+	// settings.put(SettingType.cmd_outputPath, "output/category");
+	// }
+	// };
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 		// TODO Auto-generated method stub
@@ -96,51 +180,54 @@ public class Category {
 		Job job = Job.getInstance(conf, "Category type and price");
 		job.setJarByClass(Category.class);
 		job.setMapperClass(CategoryMapper.class);
-		// job.setCombinerClass(CategoryCombiner.class);
+		job.setCombinerClass(CategoryCombiner.class);
 		job.setReducerClass(CategoryReducer.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
 
-		FileInputFormat.addInputPath(job, new Path(settings.get(CMD.cmd_dataPath)));
-		FileOutputFormat.setOutputPath(job, new Path(settings.get(CMD.cmd_outputPath)));
+		FileInputFormat.addInputPath(job, new Path(settings.get(SettingType.cmd_dataPath)));
+		FileOutputFormat.setOutputPath(job, new Path(settings.get(SettingType.cmd_outputPath)));
 
-		// 判断output文件夹是否存在，如果存在则删除
-		Path path = new Path(settings.get(CMD.cmd_outputPath));// 取第1个表示输出目录参数（第0个参数是输入目录）
-		FileSystem fileSystem = path.getFileSystem(conf);// 根据path找到这个文件
+		// Determine if the output folder exists, delete if it exists
+		Path path = new Path(settings.get(SettingType.cmd_outputPath));
+		FileSystem fileSystem = path.getFileSystem(conf);// Find this file
+															// according to path
 		if (fileSystem.exists(path)) {
-			fileSystem.delete(path, true);// true的意思是，就算output有东西，也一带删除
+			fileSystem.delete(path, true);// True means that even if there is
+											// something in the output, it is
+											// deleted.
 		}
 
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 
 	}
 
+	// Get arguments and prase to settings.
 	public static void praseArgs(String[] args) {
 		// add default setting
-		settings.put(CMD.cmd_dataPath, "GooglePlayStoreDataTest");
-		settings.put(CMD.cmd_outputPath, "output/category");
+		settings.put(SettingType.cmd_dataPath, "GooglePlayStoreDataTest");
+		settings.put(SettingType.cmd_outputPath, "output/category");
 
 		ArrayList<String> argsList = new ArrayList<String>(Arrays.asList(args));
-//		System.out.println(settings);
 
 		for (int i = 0; i < argsList.size(); i++) {
-			// System.out.println( argsList.get(i));
+
 			switch (argsList.get(i)) {
-			case "-output":
+			case "-output": // argument match, set up output path, if do not
+							// follow a cmd, then path not empty
 				if ((i + 1) < argsList.size() && !checkCMD(argsList
 						.get(i + 1))/* && checkPath(argsList.get(i + 1)) */)
-					settings.put(CMD.cmd_outputPath, argsList.get(i + 1));
+					settings.put(SettingType.cmd_outputPath, argsList.get(i + 1));
 				break;
-			case "-data":
+			case "-data": // same but for data path
 				if ((i + 1) < argsList.size() && !checkCMD(argsList
 						.get(i + 1))/* && checkPath(argsList.get(i + 1)) */)
-					settings.put(CMD.cmd_dataPath, argsList.get(i + 1));
+					settings.put(SettingType.cmd_dataPath, argsList.get(i + 1));
 				break;
 			}
 
 		}
 
-//		System.out.println(settings);
 	}
 
 	// public static boolean checkPath(String path) {
@@ -148,9 +235,10 @@ public class Category {
 	// return match(pattern, path);
 	// }
 
-	public static boolean checkCMD(String path) {
+	// check the string is a cmd or not
+	public static boolean checkCMD(String cmd) {
 		String pattern = "-.*";
-		return match(pattern, path);
+		return match(pattern, cmd);
 	}
 
 	private static boolean match(String regex, String str) {
